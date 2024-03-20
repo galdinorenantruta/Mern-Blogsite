36ecda8f31a0cc12f6070982af68b93ca4de43cd
galdinorenantruta <galdinotrutarenan@gmail.com>
2024-03-20 19:06:14 -0300

    finalizando api

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -3,6 +3,7 @@ import mongoose  from 'mongoose'
 const UserSchema = new mongoose.Schema({
     username: {type:String, required: true, },
     password: {type:String, required: true},
+    savedPosts: [{type:String, ref:"posts" }]
 })
 
 export const userModel = mongoose.model("users", UserSchema)
--- server/src/routes/blog-posts.js
+++ server/src/routes/blog-posts.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import mongoose from 'mongoose'
+
 import {PostsModel} from '../models/blogPosts.js'
+import { userModel } from '../models/users.js'
     
 const router = express.Router()
 
@@ -25,5 +26,43 @@ router.post('/', async(req, res) => {
     }
 })
 
+router.put('/', async(req, res) => {
+    try{
+    const post = await PostsModel.findById(req.body.postId)
+    const user = await userModel.findById(req.body.userId)
+    user.savedPosts.push(post)
+    await user.save()
+    res.json({savedPosts: user.savedPosts})
+    }catch(err){
+        res.json(err)
+    }
+})
+
+router.get('/savedPosts/ids', async (req, res) => {
+        try{
+            const user = await userModel.findById(req.body.userId)
+
+            res.json({savedPosts:user?.savedPosts})
+
+        }catch(err){
+            res.json(err)
+        }
+
+})
+
+
+router.get('/savedPosts', async (req, res) => {
+    try{
+        const user = await userModel.findById(req.body.userId)
+        const savedPosts = await userModel.find({
+            _id:{$in: user.savedPosts}
+        })
+        res.json({savedPosts})
+
+    }catch(err){
+        res.json(err)
+    }
+
+})
 
 export {router as postRouter}   
